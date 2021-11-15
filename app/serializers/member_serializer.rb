ab60564820d37be10a613695b20e76b4e1462be2
George Dilthey <george.dilthey@gmail.com>
2021-11-15 15:24:56 -0500

    fixed serializers

--- app/serializers/member_serializer.rb
+++ app/serializers/member_serializer.rb
@@ -1,3 +1,3 @@
 class MemberSerializer < ActiveModel::Serializer
-   attributes :mid, :image_225x275, :first_name, :last_name, :roles
+   attributes :mid, :image_225x275, :first_name, :last_name
 end
