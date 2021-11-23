22d763a5047dfbdaf8fa3ae67314cbd9865ccc4f
George Dilthey <george.dilthey@gmail.com>
2021-11-23 18:34:08 -0500

    completed USer signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,15 +3,17 @@ class UsersController < ApplicationController
     def create
         user = User.new(user_params)
         if user.save
-            render json: user
+          render_user_with_token(user)
         else
-            render json: {errors: user.errors.full_messages.to_sentence}, status: :unprocessable_entity
+          render json: {errors: user.errors.full_messages.to_sentence}, status: :unprocessable_entity
         end
     end
-
+  
+  
+  
     private
-
+  
     def user_params
-        params.permit(:email, :first_name, :last_name, :password)
+      params.permit(:email, :first_name, :last_name, :password)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   resources :users
-  resources :bills
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace 'api' do
     namespace 'v1' do
       resources :members, param: :mid
       resources :roles
+      resources :bills
     end
   end
 end
