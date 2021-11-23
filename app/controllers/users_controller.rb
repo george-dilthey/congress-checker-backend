ae06ee0abdb9a8b29ae6eacefadb86378c6e7ffc
George Dilthey <george.dilthey@gmail.com>
2021-11-23 18:53:28 -0500

    fixed user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
     def create
         user = User.new(user_params)
         if user.save
-          render_user_with_token(user)
+          render json: user
         else
           render json: {errors: user.errors.full_messages.to_sentence}, status: :unprocessable_entity
         end
