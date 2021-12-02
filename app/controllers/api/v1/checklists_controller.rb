a2052e0cfa9c42b7513c449a8e05e1f8d23ef3b5
George Dilthey <george.dilthey@gmail.com>
2021-12-01 21:47:23 -0500

    updated checklist controller to remove from checklsit

--- app/controllers/api/v1/checklists_controller.rb
+++ app/controllers/api/v1/checklists_controller.rb
@@ -11,7 +11,11 @@ module Api
                 checklist = Checklist.find_by(id: params[:id])
                 member = Member.find_by(mid: params[:mid])
                 if checklist && member
-                    checklist.members << member
+                    if params[:removeMember]
+                        checklist.members.delete(member)
+                    else
+                        checklist.members << member
+                    end
                     render json: {user: UserSerializer.new(checklist.user), token: encode_token(checklist.user.id)}
                 else
                     render json: {errors: user.errors.full_messages.to_sentence}, status: :unprocessable_entity
--- app/models/checklist.rb
+++ app/models/checklist.rb
@@ -2,5 +2,4 @@ class Checklist < ApplicationRecord
   belongs_to :user
   has_many :checklist_members
   has_many :members, -> { distinct }, through: :checklist_members
-
 end
