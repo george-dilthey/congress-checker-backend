0dc75bf055df8fd22a5c19871bc3efda3ced2d2f
George Dilthey <george.dilthey@gmail.com>
2021-12-01 23:22:03 -0500

    updated user and session controller to account for added members when login/signup

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,10 @@ class SessionsController < ApplicationController
     def create
         user = User.find_by_email(params[:email])
         if user && user.authenticate(params[:password])
+            if params[:mid]
+                member = Member.find(params[:mid])
+                user.checklists.first.members << member
+            end
             render json: {user: UserSerializer.new(user), token: encode_token(user.id)}
         else
             render json: {errors: "Invalid email and/or password. Please try again."}, status: :forbidden
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,11 @@ class UsersController < ApplicationController
     def create
         user = User.new(user_params)
         if user.save
+            user.checklists.create
+            if params[:mid]
+              member = Member.find(params[:mid])
+              user.checklists.first.members << member
+            end
             render json: {user: UserSerializer.new(user), token: encode_token(user.id)}
         else
           render json: {errors: user.errors.full_messages.to_sentence}, status: :unprocessable_entity
@@ -13,5 +18,9 @@ class UsersController < ApplicationController
   
     def user_params
       params.permit(:email, :first_name, :last_name, :password)
+    end
+
+    def member_params
+      params.permit(:mid)
     end
 end
